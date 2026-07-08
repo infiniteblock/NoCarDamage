29c1c4cc549fbf176091660c838de02be5a817fe
infiniteblock <subaru_gt@windowslive.com>
2026-07-08 12:33:26 +1200

    Update config.cpp

--- NoCarDamage/config.cpp
+++ NoCarDamage/config.cpp
@@ -1,3 +1,15 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Original mod: NoCarDamage by blubberdiblupp
+ * Original source: https://github.com/Blubberdiblupp/NoCarDamage
+ *
+ * Modified by: Schmicky, 8/7/2026
+ * source: https://github.com/infiniteblock/NoCarDamage
+ * Changes: [Fix's for eAI,Toxic Zones,Player Damage"]
+ */
 class CfgPatches
 {
 	class NoCarDamage
@@ -14,6 +26,10 @@ class CfgMods
 	{
 		dir = "NoCarDamage";
 		name = "NoCarDamage";
+		credits="Original mod by blubberdiblupp";
+        author="SchmickZ";
+        authorID="76561198110186268";
+        version="0.1";
 		type = "mod";
 		inputs = "NoCarDamage/data/inputs.xml";
 		dependencies[] = {"Game","World","Mission"};
