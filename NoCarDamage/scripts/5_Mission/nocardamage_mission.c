135e37b6dc1488c19e47e91368d0472b593cc260
infiniteblock <subaru_gt@windowslive.com>
2026-07-08 12:43:06 +1200

    Update nocardamage_mission.c

--- NoCarDamage/scripts/5_Mission/nocardamage_mission.c
+++ NoCarDamage/scripts/5_Mission/nocardamage_mission.c
@@ -1,4 +1,16 @@
-modded class MissionServer
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Original mod: NoCarDamage by blubberdiblupp
+ * Original source: https://github.com/Blubberdiblupp/NoCarDamage
+ *
+ * Modified by: Schmicky, 8/7/2026
+ * source: https://github.com/infiniteblock/NoCarDamage
+ * Changes: [Fix's for eAI,Toxic Zones,Player Damage"]
+ */
+ modded class MissionServer
 {
 	override void OnInit()
 	{
